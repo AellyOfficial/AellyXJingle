a4855f083ce0550e2935c47e134e4a20af50d247
Ananya <90745373+anunicn47@users.noreply.github.com>
2022-11-29 06:46:44 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -80,7 +80,7 @@ START_IMG_URL = getenv("START_IMG_URL",
 "https://telegra.ph/file/27516020e3cb357a41885.png"
 PING_IMG_URL = getenv("PING_IMG_URL",
 
-"https://telegra.ph/file/56d1760224589ee370186.jpg",
+"https://te.legra.ph/file/27516020e3cb357a41885.png",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
